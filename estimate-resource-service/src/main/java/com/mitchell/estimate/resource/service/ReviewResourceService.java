377f0fe9f09cea75edb2ae599d033c91f945d598
unknown <sm100997@SM100997D.corp.int>
2015-12-07 19:00:21 -0800

    Update ApplicationPath annotation

--- estimate-resource-service/src/main/java/com/mitchell/estimate/resource/service/ReviewResourceService.java
+++ estimate-resource-service/src/main/java/com/mitchell/estimate/resource/service/ReviewResourceService.java
@@ -12,7 +12,7 @@ import com.mitchell.estimate.resource.service.endpoints.ProfileEndpoint;
 import com.mitchell.estimate.resource.service.endpoints.RepairTotalsEndpoint;
 import com.mitchell.estimate.resource.service.endpoints.VehicleEndpoint;
 
-@ApplicationPath("/review")
+@ApplicationPath("")
 public class ReviewResourceService extends Application {
 
     private Set<Object> singletons = new HashSet<Object>();
